16a5e5fecd1079aea7ac23b6b21c7a560659519c
Chrisplaysharp <cwalden@berkeley.edu>
2018-01-16 04:12:18 -0800

    chris init

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Mood Tracker</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Welcome to Mood Tracker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className='App-intro'>
+          How ya feelin?
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
