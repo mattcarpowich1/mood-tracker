1d88287f49caad4dc8dd1d796d26d9799d010908
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-01-16 22:34:58 -0800

    insert json from request body

--- db/db.go
+++ db/db.go
@@ -13,24 +13,32 @@ var (
 )
 
 type User struct {
-  username string
-  email string
-  passwordHash string
-  passwordSalt string
+  Username string
+  Email string
+  PasswordHash string
+  PasswordSalt string
   CreatedAt time.Time
-  lastLogin time.Time
+  UpdatedAt time.Time
+  LastLogin time.Time
 }
 
-func InsertUser(db *sql.DB) error {
-  sqlStatement := `  
+func InsertUser(db *sql.DB, user *User) error {
+
+  query := `  
     INSERT INTO users (username, email, passwordHash,
-    passwordSalt, createdAt, lastLogin)  
-    VALUES ($1, $2, $3, $4, $5, $6)`
-  _, err := db.Exec(sqlStatement, "mattcarpowich", "mattcarpowich@gmail.com",
-    "aaaa", "bbbb", time.Now(), time.Now())
+    passwordSalt, createdAt, updatedAt, lastLogin)  
+    VALUES ($1, $2, $3, $4, $5, $6, $7)`
+
+  _, err := db.Exec(query, user.Username, user.Email,
+    user.PasswordHash, user.PasswordSalt, user.CreatedAt, 
+    user.UpdatedAt, user.LastLogin)
+
   if err != nil {
     return err
   }
-  fmt.Println("New record!")
+
+  fmt.Println("New user added!")
+
   return nil
+  
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,42 +8,32 @@ import (
   "time"
 )
 
-type User struct {
-  Username string
-  Password string
-  CreatedAt time.Time
-}
-
 var err error
 
-func CreateUser(dbCon *sql.DB) http.HandlerFunc {
-
+func AddUser(dbCon *sql.DB) http.HandlerFunc {
   fn := func(w http.ResponseWriter, r *http.Request) {
 
-    // initialize empty user
-    user := User{}
+    user := db.User{}
 
-    // parse json request body and use it to set fields on user
-    // user is passed as a pointer variable so its fields can be modified 
     err := json.NewDecoder(r.Body).Decode(&user)
     if err != nil {
       panic(err)
     }
 
-    err = db.InsertUser(dbCon)
+    user.CreatedAt = time.Now().Local()
+    user.UpdatedAt = user.CreatedAt
+    user.LastLogin = user.UpdatedAt
+
+    err = db.InsertUser(dbCon, &user)
     if err != nil {
       panic(err)
     }
 
-    // set created at field on user to current local time
-    user.CreatedAt = time.Now().Local()
-
-    // MARSHAL, or convert user object back to json and 
-    // write to response
     userJson, err := json.Marshal(user)
     if err != nil {
       panic(err)
     }
+    
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
     w.Write(userJson)
