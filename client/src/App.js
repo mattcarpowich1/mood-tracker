b39c8458569a3a609fd11fca0c41f16891a06a7f
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-01-20 15:26:30 -0800

    jwt auth somehwat implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react'
 import './App.css'
 import Main from './components/Main'
 import Nav from './components/Nav'
-// import Home from './pages/Home'
-// import Footer from './components/Footer'
 
 class App extends Component {
 
   render() {
+
     return (
       <div className='App'>
-        <Nav />
-
         <Main />
-
-      {/*<Footer />*/}
       </div>
     )
+    
   }
 }
 
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,62 +1,97 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Login.css'
 import Button from '../../components/Button'
 
-const Login = props => {
+class Login extends Component {
 
-  let username, password
+  state = {
+    hover: false
+  }
 
-  return (
-    <div>
+  componentDidMount() {
+    this.username.focus()
+  }
+
+  render() {
+
+    let username, password
+
+    const buttonStyle = {
+      fontFamily: 'inherit',
+      fontSize: '16px',
+      width: '192px',
+      padding: '8px 0px',
+      border: '1px solid #999',
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      color: '#999'
+    }
+
+    return (
       <div>
+        <div>
 
-        <form 
-          onSubmit={
-            e => {
-              e.preventDefault()
-              props.handler(
-                username.value,
-                password.value
-              )
-            }          
-          }
-        >
-          <div className='field'>
-            <label>Username</label>
-            <input name='username' 
-              type='text'
-              ref={ node => {
-                  username = node
-                } }
-            ></input>
-          </div>
+          <form 
+            onSubmit={
+              e => {
+                e.preventDefault()
+                this.props.handler(
+                  username.value,
+                  password.value
+                )
+              }          
+            }
+          >
+            <div className='field'>
+              <label>Username</label>
+              <input name='username' 
+                type='text'
+                ref={ node => {
+                    username = node
+                    this.username = node
+                  } }
+              >
+              </input>
+            </div>
 
-          <div className='field'>
-            <label>Password</label>
-            <input name='password' 
-              type='password' 
-              ref={ node => {
-                  password = node
-                } }
-            ></input>
-          </div>
+            <div className='field'>
+              <label>Password</label>
+              <input name='password' 
+                type='password' 
+                ref={ node => {
+                    password = node
+                  } }
+              ></input>
+            </div>
 
-          <div className='field'>
-            <button type='submit'>
-              Login
-            </button>
-          </div>
+            <div className='field'>
+              <button onMouseEnter={() => {
+                this.setState({
+                  hover: true
+                })
+              }} 
+                onMouseLeave={() => {
+                  this.setState({
+                    hover: false
+                  })
+                }}
+                style={ buttonStyle }
+                className={ this.state.hover ? 'hover' : '' }
+                type='submit'>
+                Login
+              </button>
+            </div>
 
-          {
-            props.error ? <h2>{ props.error }</h2>
-              : null
-          }
+            {
+              this.props.error ? <h2>{ this.props.error }</h2>
+                : null
+            }
 
-        </form>
+          </form>
 
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Login
